a8936927bd00778cc6d60d174482a9e1743d65b9
Axel Loupias <axelloupias@gmail.com>
2023-07-22 16:57:42 +0200

    Add obtainSelector method
    Signed-off-by: Axel Loupias <axelloupias@gmail.com>

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,14 +5,23 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 
-contract MyToken is ERC20, ERC165 {
+interface MyInterface {
+    function obtainSelector() external pure returns (bytes4);
+}
+
+contract MyToken is ERC20, ERC165, MyInterface {
     constructor() ERC20('MyToken', 'MTK') {}
 
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual override returns (bool) {
         return
+            interfaceId == type(MyInterface).interfaceId ||
             interfaceId == type(IERC20).interfaceId ||
             super.supportsInterface(interfaceId);
+    }
+
+    function obtainSelector() external pure override returns (bytes4) {
+        return MyInterface.obtainSelector.selector;
     }
 }
--- test/MyToken.test.ts
+++ test/MyToken.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { ethers } from 'hardhat'
-import { IERC20__factory, MyToken } from '../typechain-types'
+import { IERC20__factory, MyInterface__factory, MyToken } from '../typechain-types'
 import { getInterfaceID } from '../scripts/utils'
 
 describe('MyToken Tests', () => {
@@ -18,11 +18,17 @@ describe('MyToken Tests', () => {
         await loadFixture(deployFixture)
     })
 
-    it('Check if MyToken supports IERC20', async function () {
+    it('Check if MyToken supports IERC20', async () => {
         const IERC20 = IERC20__factory.createInterface()
         const IERC20InterfaceId = getInterfaceID(IERC20)
 
         expect(await myToken.supportsInterface(IERC20InterfaceId)).to.be.true;
 
+    })
+
+    it('Check totalSupply selector', async () => {
+        const myInterface = MyInterface__factory.createInterface()
+        const sighash = myInterface.getFunction('obtainSelector').selector
+        expect(await myToken.obtainSelector()).to.be.equal(sighash)
     })
 })
